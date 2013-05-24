7f717bb2071466be441baaa8464e373d08311bb7
Alí Adame Cantorán <aliadame15@gmail.com>
2013-05-23 18:43:32 -0700

    Versión para instalar en Root de XAMPP

--- inc/head.php
+++ inc/head.php
@@ -1,4 +1,4 @@
 <link type="text/css" rel="stylesheet" href="<?php echo "http://".$_SERVER['SERVER_NAME']."/"; ?>css/style.css"/>
-<script type='text/javascript' src='<?php echo "http://".$_SERVER['SERVER_NAME']."/js/jquery.min.js'></script>
-<script type='text/javascript' src='<?php echo "http://".$_SERVER['SERVER_NAME']."/js/custom.js" ?>'></script>
-<script type='text/javascript' src='<?php echo "http://".$_SERVER['SERVER_NAME']."/js/jquery.tinycarousel.min.js" ?>'></script>
+<script type='text/javascript' src='<?php echo "http://".$_SERVER['SERVER_NAME']; ?>/js/jquery.min.js'></script>
+<script type='text/javascript' src='<?php echo "http://".$_SERVER['SERVER_NAME']; ?>/js/custom.js'></script>
+<script type='text/javascript' src='<?php echo "http://".$_SERVER['SERVER_NAME']; ?>/js/jquery.tinycarousel.min.js'></script>
